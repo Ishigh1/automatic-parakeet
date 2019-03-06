b6cb90517aff433cf4e47371e8d44918f7c8bb53
Ishigh1 <45936615+Ishigh1@users.noreply.github.com>
2019-03-06 12:39:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('message', msg => {
 		var category ;
 		if ((category = msg.guild.channels.find(channel => channel.type == category && channel.name == message[2])) == undefined)
 		{
-			guild.createChannel(message[2], 'category')
+			msg.guild.createChannel(message[2], 'category')
   			.then(function(category) {msg.guild.createChannel(message[1], 'text', [{
   				id: role,
   				allow: ['SEND_MESSAGES', 'READ_MESSAGES'],
