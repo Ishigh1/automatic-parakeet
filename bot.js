ad9a5f2c68a7732b160fbc352b6afa5dccdcd484
Ishigh1 <45936615+Ishigh1@users.noreply.github.com>
2019-03-06 12:50:39 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,26 +9,27 @@ client.on('message', msg => {
 	var message = msg.toString().split(" ");
 	if(message[0] == "!newchan")
 	{
-		role = msg.guild.createRole({name: message[1]});
-		console.log(role.id);
-		var category ;
-		if ((category = msg.guild.channels.find(channel => channel.type == "category" && channel.name == message[2])) == undefined)
-		{
-			msg.guild.createChannel(message[2], 'category')
-  			.then(function(category) {msg.guild.createChannel(message[1], 'text', [{
-  				id: role.id,
-  				allow: ['SEND_MESSAGES', 'READ_MESSAGES'],
-				}])
-				.then(function(chan) {chan.setParent(category)})
-			})
-		}
-		else
-		{
-			category => msg.guild.createChannel(message[1], 'text', [{
-  				id: role.id,
-  				allow: ['SEND_MESSAGES', 'READ_MESSAGES'],
-				}])
-				.then(function(chan) {chan.setParent(category)})
+		msg.guild.createRole({name: message[1]}).then(function (role) {
+			console.log(role.id);
+			var category ;
+			if ((category = msg.guild.channels.find(channel => channel.type == "category" && channel.name == message[2])) == undefined)
+			{
+				msg.guild.createChannel(message[2], 'category')
+  				.then(function(category) {msg.guild.createChannel(message[1], 'text', [{
+  					id: role.id,
+	  				allow: ['SEND_MESSAGES', 'READ_MESSAGES'],
+					}])
+					.then(function(chan) {chan.setParent(category)})
+				})
+			}
+			else
+			{
+				category => msg.guild.createChannel(message[1], 'text', [{
+  					id: role.id,
+  					allow: ['SEND_MESSAGES', 'READ_MESSAGES'],
+					}])
+					.then(function(chan) {chan.setParent(category)})
+			}
 		}
 	}
 })
